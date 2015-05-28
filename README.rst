860eb759c20c7297811090547dc61fc2cc778f69
Jonas Baumann <jone@jone.ch>
2015-05-28 17:21:13 +0200

    create package.

---
+++ README.rst
@@ -0,0 +1,41 @@
+.. contents:: Table of Contents
+
+
+Installation
+============
+
+Add the package as dependency to your setup.py:
+
+.. code:: python
+
+  setup(...
+        install_requires=[
+          ...
+          'ftw.theming',
+        ])
+
+or to your buildout configuration:
+
+.. code:: ini
+
+  [instance]
+  eggs += ftw.theming
+
+and rerun buildout.
+
+
+
+Links
+=====
+
+- Github: https://github.com/4teamwork/ftw.theming
+- Issues: https://github.com/4teamwork/ftw.theming/issues
+- Pypi: http://pypi.python.org/pypi/ftw.theming
+- Continuous integration: https://jenkins.4teamwork.ch/search?q=ftw.theming
+
+Copyright
+=========
+
+This package is copyright by `4teamwork <http://www.4teamwork.ch/>`_.
+
+``ftw.theming`` is licensed under GNU General Public License, version 2.
---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1.0.dev0'
+
+tests_require = [
+    ]
+
+
+setup(name='ftw.theming',
+      version=version,
+      description="",
+      long_description=open("README.rst").read() + "\n" + open(
+        os.path.join("docs", "HISTORY.txt")).read(),
+
+      classifiers=[
+        "Environment :: Web Environment",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+
+      keywords='ftw theming',
+      author='4teamwork AG',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.theming',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'Plone',
+        'setuptools',
+        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
