6cda77d4e6c4bcf1ddd305f1bd9fed751a52c78f
Jonas Baumann <jone@jone.ch>
2015-06-01 17:19:46 +0200

    implement caching of compiled css.

--- ftw/theming/browser/theming_css.py
+++ ftw/theming/browser/theming_css.py
@@ -1,7 +1,41 @@
 from ftw.theming.interfaces import ISCSSCompiler
+from operator import methodcaller
+from plone.app.layout.navigation.root import getNavigationRootObject
+from plone.memoize import ram
+from plone.memoize.interfaces import ICacheChooser
 from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
 from zope.component import getMultiAdapter
+from zope.component import getUtility
+
+
+def compute_css_bundle_hash(context):
+    """This method creates a hash of the current CSS resource registry.
+    We need this in order to detect a bundle recook and flush our caches.
+    We do this by using the hash of the currently cooked bundles as part of
+    our cache key.
+    """
+    portal_css = getToolByName(context, 'portal_css')
+    theme_resources = dict(reduce(list.__add__,
+                                  map(methodcaller('items'),
+                                      portal_css.concatenatedResourcesByTheme.values())))
+    keys = theme_resources.keys()
+    map(keys.extend, theme_resources.values())
+    return str(hash(frozenset(keys)))
+
+
+def get_css_cache_key(func, self):
+    if self._debug_mode_enabled():
+        raise ram.DontCache
+
+    if self._no_cache_header_present():
+        self.invalidate_cache()
+
+    portal = getToolByName(self.context, 'portal_url').getPortalObject()
+    navroot = getNavigationRootObject(self.context, portal)
+    key = ['/'.join(navroot.getPhysicalPath()),
+           compute_css_bundle_hash(self.context)]
+    return '.'.join(key)
 
 
 class ThemingCSSView(BrowserView):
@@ -10,10 +44,21 @@ class ThemingCSSView(BrowserView):
         response = self.request.response
         response.setHeader('Content-Type', 'text/css; charset=utf-8')
         response.setHeader('X-Theme-Disabled', 'True')
+        response.enableHTTPCompression(REQUEST=self.request)
         return self.get_css()
 
+    @ram.cache(get_css_cache_key)
     def get_css(self):
-        cssregistry = getToolByName(self.context, 'portal_css')
-        debug = bool(cssregistry.getDebugMode())
         compiler = getMultiAdapter((self.context, self.request), ISCSSCompiler)
-        return compiler.compile(debug=debug)
+        return compiler.compile(debug=self._debug_mode_enabled())
+
+    def invalidate_cache(self):
+        cache = getUtility(ICacheChooser)('{0}.get_css'.format(__name__))
+        cache.ramcache.invalidateAll()
+
+    def _debug_mode_enabled(self):
+        cssregistry = getToolByName(self.context, 'portal_css')
+        return bool(cssregistry.getDebugMode())
+
+    def _no_cache_header_present(self):
+        return self.request.getHeader('Cache-Control') == 'no-cache'
--- ftw/theming/tests/test_theming_css_view.py
+++ ftw/theming/tests/test_theming_css_view.py
@@ -1,10 +1,26 @@
 from ftw.testbrowser import browsing
+from ftw.theming.interfaces import ISCSSCompiler
 from ftw.theming.tests import FunctionalTestCase
 from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
+from zope.component import adapts
+from zope.component import provideAdapter
+from zope.interface import implements
+from zope.interface import Interface
 import transaction
 
 
 class TestThemingCSSView(FunctionalTestCase):
+
+    def setUp(self):
+        super(TestThemingCSSView, self).setUp()
+        self.view = self.portal.restrictedTraverse('theming.css')
+        self.view.invalidate_cache()
+        self.portal_css = getToolByName(self.portal, 'portal_css')
+
+    def tearDown(self):
+        super(TestThemingCSSView, self).tearDown()
+        self.portal_css.setDebugMode(False)
 
     @browsing
     def test_compiles_anonymously(self, browser):
@@ -15,12 +31,66 @@ class TestThemingCSSView(FunctionalTestCase):
 
     @browsing
     def test_css_is_registered(self, browser):
-        portal_css = getToolByName(self.portal, 'portal_css')
-        portal_css.setDebugMode(True)
-        transaction.commit()
-
+        self.portal_css.setDebugMode(True)
         browser.open()
         css_urls = [node.attrib.get('href') for node in browser.css('link')]
         self.assertEquals(
             ['http://nohost/plone/portal_css/Sunburst%20Theme/theming.css'],
             filter(lambda url: url.endswith('theming.css'), css_urls))
+
+    def test_css_is_cached(self):
+        self.register_compiler_mock()
+        self.assertEquals(1, self.view())
+        self.assertEquals(1, self.view(), 'Results should be cached but are not.')
+
+    def test_invalidating_cache(self):
+        self.register_compiler_mock()
+        self.assertEquals(1, self.view())
+        self.assertEquals(1, self.view(), 'Cache does not work...')
+        self.view.invalidate_cache()
+        self.assertEquals(2, self.view(), 'invalidate_cache() did not invalidate cache')
+        self.assertEquals(2, self.view(), 'Cache does not work...')
+
+    def test_debug_mode_does_not_cache(self):
+        self.register_compiler_mock()
+        self.assertEquals(1, self.view())
+        self.assertEquals(1, self.view(), 'Cache does not work...')
+        self.portal_css.setDebugMode(True)
+        self.assertEquals(2, self.view(), 'Caching should be disabled in debug mode.')
+        self.assertEquals(3, self.view(), 'Caching should be disabled in debug mode.')
+
+    def test_no_cache_header_disables_caching(self):
+        self.register_compiler_mock()
+        self.assertEquals(1, self.view())
+        self.assertEquals(1, self.view(), 'Cache does not work...')
+        self.request.environ['CACHE_CONTROL'] = 'no-cache'
+        self.assertEquals(2, self.view(), 'no-cache header does not work')
+        self.assertEquals(3, self.view(), 'no-cache header does not work')
+        self.request.environ['CACHE_CONTROL'] = 'no-transform'
+        self.assertEquals(3, self.view(),
+                          'no-cache header should flush the cache for later requests.')
+        self.assertEquals(3, self.view(),
+                          'no-cache header should flush the cache for later requests.')
+
+    def test_recooking_portal_css_flushes_cache(self):
+        self.register_compiler_mock()
+        self.assertEquals(1, self.view())
+        self.assertEquals(1, self.view(), 'Cache does not work...')
+        self.portal_css.cookResources()
+        self.assertEquals(2, self.view(), 'Recooking portal_css should flush cache.')
+
+    def register_compiler_mock(self):
+        sitemanager = self.portal.getSiteManager()
+        COMPILER_MOCK_DATA = {'counter': 0}
+
+        @sitemanager.registerAdapter
+        class CompilerMock(object):
+            implements(ISCSSCompiler)
+            adapts(IPloneSiteRoot, Interface)
+
+            def __init__(self, context, request):
+                pass
+
+            def compile(self, debug=False):
+                COMPILER_MOCK_DATA['counter'] += 1
+                return COMPILER_MOCK_DATA['counter']
