3e2656430e03268ff6273dbd23935ef0b01740bf
Jonas Baumann <jone@jone.ch>
2015-06-11 10:27:33 +0200

    add base profile, which will replace the plone css.

---
+++ ftw/theming/header.py
@@ -0,0 +1,14 @@
+from ftw.theming.interfaces import IThemingLayer
+
+
+def set_ftw_theming_header(object, event):
+    """Set an HTTP_X_FTW_THEMING header in the request when ftw.theming is enabled.
+
+    For enabling ftw.theming, the ``ftw.theming:default`` profile must be installed,
+    enabling the required request layer.
+    """
+
+    request = event.request
+    if not IThemingLayer.providedBy(request):
+        return
+    request.environ['HTTP_X_FTW_THEMING'] = True
