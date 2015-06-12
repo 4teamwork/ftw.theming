eee89a3b7c90742cb5fadb852e58f37887f49090
Jonas Baumann <jone@jone.ch>
2015-06-12 18:33:43 +0200

    pep8.

--- ftw/theming/header.py
+++ ftw/theming/header.py
@@ -4,8 +4,8 @@ from ftw.theming.interfaces import IThemingLayer
 def set_ftw_theming_header(object, event):
     """Set an HTTP_X_FTW_THEMING header in the request when ftw.theming is enabled.
 
-    For enabling ftw.theming, the ``ftw.theming:default`` profile must be installed,
-    enabling the required request layer.
+    For enabling ftw.theming, the ``ftw.theming:default`` profile must
+    be installed, enabling the required request layer.
     """
 
     request = event.request
